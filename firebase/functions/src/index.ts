ffb2b68d58c93d35b847cdc4e156b3819aa5c2d1
keepgo-studio <keepgo.studio@gmail.com>
2024-03-06 19:33:59 +0900

    Update: Firebase Functions complete

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,19 +1,85 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+import * as Admin from "firebase-admin";
+import * as Functions from "firebase-functions";
+import axios from "axios";
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+Admin.initializeApp();
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+const API_URL = "https://api.openweathermap.org";
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const IS_DEV = process.env.FUNCTIONS_EMULATOR;
+
+const middlewareSetCORS = (handler: Function) => async (req: Functions.https.Request, res: Functions.Response<any>) => {
+  const allowedOrigins = IS_DEV ? [
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
+  ] : [
+    "https://keepgo-studio.github.io/ios-standby"
+  ];
+
+  if (!req.headers.origin) {
+    res.status(400).send("Cannot recognize origin");
+    return;
+  }
+
+  const url = allowedOrigins.find((url) => url === req.headers.origin);
+
+  res.header({
+    "Access-Control-Allow-Origin": url,
+  });
+
+  return handler(req, res);
+}
+
+export const checkOrigin = Functions.region(
+  "asia-northeast3"
+).https.onRequest(middlewareSetCORS(async (_: any, res: Functions.Response<any>) => {
+  res.status(200).send("origin satisfied");
+}));
+
+export const getCurrentWeather = Functions.region(
+  "asia-northeast3"
+).https.onRequest(middlewareSetCORS(async (req: Functions.https.Request, res: Functions.Response<any>) => {
+  const { lat, lon } = req.query;
+
+  if (!lat || !lon) {
+    res.status(400).send("Wrong Parameter enter");
+    return;
+  }
+
+  const data = await axios.get(`${API_URL}/data/2.5/weather`, {
+    params: {
+      lat,
+      lon,
+      appid: process.env.API_ID,
+      limit: 5
+    }
+  })
+  .then(val => val.data)
+  .catch(() => {});
+
+  res.status(200).send(data);
+}));
+
+export const getCurrentGeo = Functions.region(
+  "asia-northeast3"
+).https.onRequest(middlewareSetCORS(async (req: Functions.https.Request, res: Functions.Response<any>) => {
+  const { lat, lon } = req.query;
+
+  if (!lat || !lon) {
+    res.status(400).send("Wrong paramter enter");
+    return;
+  }
+  
+  const data = await axios.get(`${API_URL}/geo/1.0/reverse`, {
+    params: {
+      lat,
+      lon,
+      appid: process.env.API_ID,
+      limit: 5
+    }
+  })
+  .then(val => val.data)
+  .catch(() => []);
+
+  res.status(200).send(data);
+}))
